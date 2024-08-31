1f4f5e4c54c997006aaac33d86eead41cd14f4ae
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-08-31 10:47:39 +0530

    create array

--- src/main/java/com/example/aadinclass/controller/FoodController.java
+++ src/main/java/com/example/aadinclass/controller/FoodController.java
@@ -1,8 +1,20 @@
 package com.example.aadinclass.controller;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+
 @WebServlet(urlPatterns = "/foods")
 public class FoodController extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String orderedFood = req.getParameter("foodName");
+        String foodAr = orderedFood;
 
+    }
 }
