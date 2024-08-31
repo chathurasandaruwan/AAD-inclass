32e8c2ae4ae0987a3d92619aafb3f8d5e66bea6c
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-08-31 10:41:07 +0530

    first commit

---
+++ src/main/java/com/example/aadinclass/controller/FoodController.java
@@ -0,0 +1,8 @@
+package com.example.aadinclass.controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+@WebServlet(urlPatterns = "/foods")
+public class FoodController extends HttpServlet {
+
+}
